7b43e2181612fc3375579758f51549c8e91c6079
Henzo <henzommatos@gmail.com>
2026-07-14 19:25:14 -0300

    Commit inicial

---
+++ projeto.py
@@ -0,0 +1,187 @@
+"""
+
+Trabalho Mini CPS: Sistema de Controle de Estoque - Hamburgueria
+
+"""
+import sqlite3
+
+conexao = sqlite3.connect("loja.db")
+cursor = conexao.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS produtos (
+    id_produto   INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome_produto TEXT NOT NULL,
+    saldo INTEGER DEFAULT 0,
+    estoque_minimo INTEGER DEFAULT 5
+)""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS categorias (
+    id_categoria   INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome_categoria TEXT NOT NULL
+)""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS produto_categoria (
+    id_produto_categoria INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_categoria INTEGER,
+    id_produto INTEGER,
+    FOREIGN KEY (id_categoria) REFERENCES categorias (id_categoria),
+    FOREIGN KEY (id_produto) REFERENCES produtos (id_produto)
+)""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS movimentacoes (
+    id_movimentacao INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_categoria INTEGER,
+    id_produto INTEGER,
+    quantidade INTEGER,
+    tipo_movimentacao TEXT,
+    data_movimentacao TEXT DEFAULT (datetime('now','localtime')),
+    FOREIGN KEY (id_categoria) REFERENCES categorias (id_categoria),
+    FOREIGN KEY (id_produto) REFERENCES produtos (id_produto)
+)""")
+
+conexao.commit()
+
+while True:
+    print("")
+    print("=== HAMBURGUERIA - CONTROLE DE ESTOQUE ===")
+    print("1 - Cadastrar produto")
+    print("2 - Relatório de inventário")
+    print("3 - Registrar Movimentação (Saída)")
+    print("4 - Registrar Movimentação (Entrada)")
+    print("5 - Cadastar Categoria")
+    print("0 - Sair")
+    opcao = input("Opção: ")
+
+
+    if opcao == "1":
+        nome = input("Nome do produto: ")
+        categorias = cursor.execute("SELECT nome_categoria FROM categorias").fetchall()
+        lista_categorias = [linha[0] for linha in categorias]
+
+        while True:
+            print("Categorias Disponíveis:")
+            for cat in lista_categorias:
+                print(f"- {cat}")
+
+            categoria = input("Selecione a Categoria: ")
+
+            if categoria not in lista_categorias:
+                print("Categoria inválida.")
+                continue
+            else:
+                quantidade = int(input("Quantidade inicial: "))
+                cursor.execute(
+                    "INSERT INTO produtos (nome_produto, saldo) VALUES (?, ?)",
+                    (nome, quantidade)
+                )
+                conexao.commit()
+                print("Produto cadastrado!")
+                break
+
+    elif opcao == "2":
+        produtos = cursor.execute("SELECT nome_produto, saldo FROM produtos").fetchall()
+        print("Produtos Cadastrados: \n")
+        for p in produtos:
+            print(p[0], "- saldo:", p[1])
+
+
+    elif opcao == "3":
+        produtos = cursor.execute("SELECT id_produto, nome_produto, saldo FROM produtos").fetchall()
+        print("Produtos cadastrados (ID, Nome e Saldo Atual): ")
+
+        while True:
+            for p in produtos:
+                id_produto, nome_produto, saldo_produto = p
+                print(f" {id_produto} | {nome_produto} | {saldo_produto} ")
+
+            id = input("Digite o ID do produto que deseja movimentar o saldo: ")
+
+            linha = cursor.execute("SELECT id_produto, nome_produto, saldo FROM produtos WHERE id_produto = ?", (id,)).fetchone()
+
+            if linha is None:
+                print("Produto não encontrado. Tente novamente!")
+                continue
+            else:
+                id_produto = linha[0]
+                saldo_atual = linha[2]
+                while True:
+                    quantidade = int(input("Quantidade: "))
+
+                    if quantidade > saldo_atual:
+                        print("Movimentação não permitida: Quantidade maior que o estoque disponível.")
+                        continue
+                    else:
+                        cursor.execute(
+                            "INSERT INTO movimentacoes (id_produto, quantidade, tipo_movimentacao) VALUES (?, ?, ?)",
+                            (id_produto, quantidade, "Saída")
+                        )
+
+                        cursor.execute("UPDATE produtos SET saldo = saldo - ? WHERE id_produto = ?", (quantidade, id_produto))
+                        conexao.commit()
+                        print("Movimentação registrada!")
+
+                        situacao_produto_movimentado = cursor.execute("SELECT id_produto, nome_produto, saldo, estoque_minimo FROM produtos WHERE id_produto = ?", (id_produto,)).fetchone()
+
+                        id_produto, nome_produto, saldo_produto, estoque_minimo = situacao_produto_movimentado
+
+                        if saldo_produto <= estoque_minimo:
+                            situacao = "ALERTA: estoque baixo!"
+                        else:
+                            situacao = "OK"
+                        print("Situação do Produto Movimentado (ID, Nome, Saldo Atual e Situação): ")
+                        print(f"{id_produto} | {nome_produto} | {saldo_produto} | {situacao}")
+
+    elif opcao == "4":
+        produtos = cursor.execute("SELECT id_produto, nome_produto, saldo FROM produtos").fetchall()
+        print("Produtos cadastrados (ID, Nome e Saldo Atual): ")
+        for p in produtos:
+            id_produto, nome_produto, saldo_produto = p
+            print(f" {id_produto} | {nome_produto} | {saldo_produto} ")
+
+        id = input("Digite o ID do produto que deseja movimentar o saldo: ")
+        linha = cursor.execute("SELECT id_produto, nome_produto, saldo FROM produtos WHERE id_produto = ?", (id,)).fetchone()
+
+        if linha is None:
+            print("Produto não encontrado.")
+        else:
+            id_produto = linha[0]
+            nome_produto[1]
+            saldo_atual = linha[2]
+
+            quantidade = int(input("Quantidade: "))
+            cursor.execute(
+                "INSERT INTO movimentacoes (id_produto, quantidade, tipo_movimentacao) VALUES (?, ?, ?)",
+                (id_produto, quantidade, "Saída"))
+
+            cursor.execute("UPDATE produtos SET saldo = saldo + ? WHERE id_produto = ?", (quantidade, id_produto))
+            conexao.commit()
+            print("Movimentação registrada!")
+
+            situacao_produto_movimentado = cursor.execute("SELECT id_produto, nome_produto, saldo, estoque_minimo FROM produtos WHERE id_produto = ?", (id_produto,)).fetchone()
+
+            id_produto, nome_produto, saldo_produto, estoque_minimo = situacao_produto_movimentado
+
+            if saldo_produto <= estoque_minimo:
+                situacao = "ALERTA: estoque baixo!"
+            else:
+                situacao = "OK"
+            print("Situação do Produto Movimentado (ID, Nome, Saldo Atual e Situação): ")
+            print(f"{id_produto} | {nome_produto} | {saldo_produto} | {situacao}")
+
+    elif opcao == "5":
+        nome = input("Nome do categoria: ")
+        cursor.execute(
+            "INSERT INTO categorias (nome_categoria) VALUES (?)",
+            (nome,)
+        )
+        conexao.commit()
+        print("Categoria cadastrada!")
+
+    elif opcao == "0":
+        print("Ate logo!")
+        break
+
+    else:
+        print("Opção inválida.")
+
+conexao.close()
